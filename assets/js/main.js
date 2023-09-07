02793effa86ab65747581cfd82fad4d94db11d05
YogiSOEDA <yogiajus87@gmail.com>
2023-09-08 03:43:35 +0800

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,107 @@
+// References to DOM elements
+const prevBtn = document.querySelector('#prev-btn');
+const nextBtn = document.querySelector('#next-btn');
+const book = document.querySelector('#book');
+
+const paper1 = document.querySelector('#p1')
+const paper2 = document.querySelector('#p2')
+const paper3 = document.querySelector('#p3')
+
+const audio = document.getElementById('audio');
+const btnMute = document.getElementById('mute-btn');
+const icon = document.getElementById('icon-music');
+
+var count = 0;
+
+// Event listeners
+prevBtn.addEventListener("click", goPrevious);
+nextBtn.addEventListener("click", goNext);
+nextBtn.addEventListener("click", playAudio);
+btnMute.addEventListener("click", changeIcon);
+
+// Business Logic
+let currentState = 1;
+let numOfPapers = 3;
+let maxState = numOfPapers + 1;
+
+
+function openBook() {
+    book.style.transform = "translateX(50%)";
+    prevBtn.style.transform = "translateX(-180px)";
+    nextBtn.style.transform = "translateX(180px)";
+}
+
+function closeBook(isFirstPage) {
+    if(isFirstPage) {
+        book.style.transform = "translateX(0%)";
+    } else {
+        book.style.transform = "translateX(100%)";
+    }
+    prevBtn.style.transform = "translateX(0px)";
+    nextBtn.style.transform = "translateX(0px)";
+}
+
+function goNext() {
+    if(currentState < maxState) { 
+        switch(currentState) {
+            case 1:
+                openBook();
+                paper1.classList.add("flipped");
+                paper1.style.zIndex = 1;
+                break;
+            case 2:
+                paper2.classList.add("flipped");
+                paper2.style.zIndex = 2;
+                break;
+            case 3:
+                closeBook(false);
+                paper3.classList.add("flipped");
+                paper3.style.zIndex = 3;
+                break;
+            default: 
+                throw new Error("unkown state");    
+        }
+
+        currentState++;
+    }
+}
+
+function goPrevious() {
+    if(currentState > 1) {
+        switch(currentState) {
+            case 2:
+                closeBook(true);
+                paper1.classList.remove("flipped");
+                paper1.style.zIndex = 3;
+                break;
+            case 3:
+                paper2.classList.remove("flipped");
+                paper2.style.zIndex = 2;
+                break;
+            case 4: 
+                openBook()
+                paper3.classList.remove("flipped");
+                paper3.style.zIndex = 1;
+                break;
+        }
+
+        currentState--;
+    }
+}
+
+//audio
+function playAudio(){
+    audio.play();
+}
+
+function changeIcon(){
+    if(count == 0){
+        count = 1;
+        audio.muted = false;
+        icon.className = "fa-solid fa-volume-low";
+    }else{
+        count = 0;
+        audio.muted = true;
+        icon.className = "fa-solid fa-volume-xmark";
+    }
+}
