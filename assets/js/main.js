ea287280b4f9235008f5628eaafbc20de698366d
YogiSOEDA <yogiajus87@gmail.com>
2023-09-09 01:17:49 +0800

    add new page part2

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,8 @@ const paper9 = document.querySelector('#p9')
 const paper10 = document.querySelector('#p10')
 const paper11 = document.querySelector('#p11')
 const paper12 = document.querySelector('#p12')
+const paper13 = document.querySelector('#p13')
+const paper14 = document.querySelector('#p14')
 
 const audio = document.getElementById('audio');
 const btnMute = document.getElementById('mute-btn');
@@ -30,7 +32,7 @@ btnMute.addEventListener("click", changeIcon);
 
 // Business Logic
 let currentState = 1;
-let numOfPapers = 12;
+let numOfPapers = 14;
 let maxState = numOfPapers + 1;
 
 
@@ -99,9 +101,17 @@ function goNext() {
                 paper11.style.zIndex = 11;
                 break;
             case 12:
-                closeBook(false);
                 paper12.classList.add("flipped");
                 paper12.style.zIndex = 12;
+                break;
+            case 13:
+                paper13.classList.add("flipped");
+                paper13.style.zIndex = 13;
+                break;
+            case 14:
+                closeBook(false);
+                paper14.classList.add("flipped");
+                paper14.style.zIndex = 14;
                 break;
             default: 
                 throw new Error("unkown state");    
@@ -117,52 +127,60 @@ function goPrevious() {
             case 2:
                 closeBook(true);
                 paper1.classList.remove("flipped");
-                paper1.style.zIndex = 12;
+                paper1.style.zIndex = 14;
                 break;
             case 3:
                 paper2.classList.remove("flipped");
-                paper2.style.zIndex = 11;
+                paper2.style.zIndex = 13;
                 break;
             case 4: 
                 paper3.classList.remove("flipped");
-                paper3.style.zIndex = 10;
+                paper3.style.zIndex = 12;
                 break;
             case 5: 
                 paper4.classList.remove("flipped");
-                paper4.style.zIndex = 9;
+                paper4.style.zIndex = 11;
                 break;
             case 6: 
                 paper5.classList.remove("flipped");
-                paper5.style.zIndex = 8;
+                paper5.style.zIndex = 10;
                 break;
             case 7: 
                 paper6.classList.remove("flipped");
-                paper6.style.zIndex = 7;
+                paper6.style.zIndex = 9;
                 break;
             case 8: 
                 paper7.classList.remove("flipped");
-                paper7.style.zIndex = 6;
+                paper7.style.zIndex = 8;
                 break;
             case 9: 
                 paper8.classList.remove("flipped");
-                paper8.style.zIndex = 5;
+                paper8.style.zIndex = 7;
                 break;
             case 10: 
                 paper9.classList.remove("flipped");
-                paper9.style.zIndex = 4;
+                paper9.style.zIndex = 6;
                 break;
             case 11: 
                 paper10.classList.remove("flipped");
-                paper10.style.zIndex = 3;
+                paper10.style.zIndex = 5;
                 break;
             case 12: 
                 paper11.classList.remove("flipped");
-                paper11.style.zIndex = 2;
+                paper11.style.zIndex = 4;
                 break;
             case 13: 
-                openBook()
                 paper12.classList.remove("flipped");
-                paper12.style.zIndex = 1;
+                paper12.style.zIndex = 3;
+                break;
+            case 14: 
+                paper13.classList.remove("flipped");
+                paper13.style.zIndex = 2;
+                break;
+            case 15: 
+                openBook()
+                paper14.classList.remove("flipped");
+                paper14.style.zIndex = 1;
                 break;
         }
 
